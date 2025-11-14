2d2e73c88c592be7283f3eab79b0a3d1530b47d8
Balazs <balzs.gyngysi@gmail.com>
2025-11-14 20:04:59 +0100

    format

--- FogOfWar/temp_boat.gd
+++ FogOfWar/temp_boat.gd
@@ -1,13 +1,12 @@
 extends CharacterBody2D
 
-
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
 @onready var vision: Sprite2D = %Vision
 
-func _physics_process(_delta: float) -> void:
 
+func _physics_process(_delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
