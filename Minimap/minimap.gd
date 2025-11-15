d2d24da094ed730db9cb84b47cbeac129c24c7ab
Balazs <balzs.gyngysi@gmail.com>
2025-11-15 20:00:17 +0100

    added minimap - added fog of war and minimap to gameloopscene - player around with speed and wind power numbers

--- Minimap/minimap.gd
+++ Minimap/minimap.gd
@@ -1,8 +1,13 @@
 extends Node2D
 
 @export var player: CharacterBody2D
-@onready var minimap_camera: Camera2D = $CanvasLayer/SubViewportContainer/SubViewport/MinimapCamera
+@onready var camera_2d: Camera2D = $CanvasLayer/SubViewportContainer/SubViewport/Camera2D
+@onready var sub_viewport: SubViewport = $CanvasLayer/SubViewportContainer/SubViewport
 
-func _process(_delta: float) -> void:
-	if(player):
-		minimap_camera.position = player.position
+func _ready():
+	sub_viewport.world_2d = get_world_2d()
+	sub_viewport.render_target_update_mode = SubViewport.UPDATE_ALWAYS
+
+func _process(_delta):
+	if player:
+		camera_2d.global_position = player.global_position
