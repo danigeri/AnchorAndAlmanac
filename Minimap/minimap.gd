650c63b6374c8e5581cd89da7fb28c9aa50ebab1
Balazs <balzs.gyngysi@gmail.com>
2025-11-15 20:21:37 +0100

    lint

--- Minimap/minimap.gd
+++ Minimap/minimap.gd
@@ -4,9 +4,11 @@ extends Node2D
 @onready var camera_2d: Camera2D = $CanvasLayer/SubViewportContainer/SubViewport/Camera2D
 @onready var sub_viewport: SubViewport = $CanvasLayer/SubViewportContainer/SubViewport
 
+
 func _ready():
 	sub_viewport.world_2d = get_world_2d()
 	sub_viewport.render_target_update_mode = SubViewport.UPDATE_ALWAYS
+
 
 func _process(_delta):
 	if player:
