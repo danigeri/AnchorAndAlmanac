5c889bd317c04f6d5c7b42ee39140bd779d53077
Balazs <balzs.gyngysi@gmail.com>
2025-11-18 17:05:04 +0100

    questlog added

---
+++ Questlog/questlog.gd
@@ -0,0 +1,27 @@
+extends Node2D
+
+@onready var questlog_item: Sprite2D = $QuestlogItem
+@onready var questlog_item_2: Sprite2D = $QuestlogItem2
+@onready var questlog_item_3: Sprite2D = $QuestlogItem3
+@onready var questlog_item_4: Sprite2D = $QuestlogItem4
+@onready var questlog_item_5: Sprite2D = $QuestlogItem5
+@onready var questlog_item_6: Sprite2D = $QuestlogItem6
+
+func _ready() -> void:
+	Globals.checkpoint_collected.connect(show_questlog_item)
+
+func show_questlog_item(checkpoint_number: int) -> void:
+	print(checkpoint_number)
+	match checkpoint_number:
+		0:
+			questlog_item.show()
+		1:
+			questlog_item_2.show()
+		2:
+			questlog_item_3.show()
+		3:
+			questlog_item_4.show()
+		4:
+			questlog_item_5.show()
+		5:
+			questlog_item_6.show()
--- UI/ui.gd
+++ UI/ui.gd
@@ -4,12 +4,12 @@ var _speed_mps: float = 0
 var _sail_state: int = 0
 @onready var checkpoint_counter: Label = $UserInterface/CheckpointCounter
 @onready var debug_label: Label = $UserInterface/DebugPanel/MarginContainer/VBoxContainer/DebugLabel
+@onready var questlog: Node2D = $Questlog
 
 
 func _ready() -> void:
 	Globals.checkpoint_collected.connect(_on_checkpoint_collected)
-
-
+	
 func _on_checkpoint_collected(_type: int) -> void:
 	## temp code to show collected checkpoints on the UI
 	var collected_names = ""
@@ -34,3 +34,8 @@ func _on_ship_speed_change(speed: Variant) -> void:
 func update_text() -> void:
 	if debug_label:
 		debug_label.text = ("speed: %.1f m/s\nsail state: %d" % [_speed_mps, _sail_state])
+
+
+func _on_questlog_button_pressed() -> void:
+	questlog.visible = !questlog.visible
+	get_tree().paused = questlog.visible
