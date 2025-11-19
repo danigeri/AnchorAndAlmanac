b6e0de01f4cca2c548fec5e278bfa793291e6216
Balazs <balzs.gyngysi@gmail.com>
2025-11-19 18:07:56 +0100

    small tweeks if fog wind updates correctly

--- Wind/wind.gd
+++ Wind/wind.gd
@@ -4,14 +4,21 @@ signal update_wind(direction, power)
 
 @export var direction = Vector2.RIGHT
 @export var strength = 50
-
-@onready var timer: Timer = $Timer
+var wind_change_time_frequency = 15
 
 
 func _ready() -> void:
 	update_wind.emit(direction, strength)
-	timer.timeout.connect(_on_wind_change)
+	create_wind_timer()
 
+func create_wind_timer():
+	var timer := Timer.new()
+	timer.wait_time = wind_change_time_frequency
+	timer.one_shot = false
+	timer.autostart = true
+	add_child(timer)
+
+	timer.timeout.connect(_on_wind_change)
 
 func _on_wind_change():
 	direction = Vector2(randf_range(-1, 1), randf_range(-1, 1))
--- WindMeter/wind_meter.gd
+++ WindMeter/wind_meter.gd
@@ -3,6 +3,8 @@ extends Control
 @onready var pointer: TextureRect = $Pointer
 @onready var strength_label: Label = $Panel/StrengthLabel
 
+func _ready() -> void:
+	Wind.update_wind.connect(_on_wind_update_wind)
 
 func _on_wind_update_wind(dir: Variant, strength: Variant) -> void:
 	if pointer:
