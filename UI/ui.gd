2096179e751320062184e63e1c6bb1b87c01757e
Balazs <balzs.gyngysi@gmail.com>
2025-11-19 21:45:46 +0100

    lint

--- UI/ui.gd
+++ UI/ui.gd
@@ -12,6 +12,7 @@ func _ready() -> void:
 	## set to always so unpause is possible with InputEvent
 	process_mode = Node.PROCESS_MODE_ALWAYS
 
+
 func _input(event: InputEvent) -> void:
 	if event is InputEventKey and event.pressed:
 		if event.keycode == KEY_Q:
