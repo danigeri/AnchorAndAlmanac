33295fde15b57ea7715b9a539e5cd91c58e712c9
Csaba Ekart <csaba.ekart@gmail.com>
2025-11-30 22:02:48 +0100

    ckassuic screenshake

--- game_loop.gd
+++ game_loop.gd
@@ -9,7 +9,7 @@ var endgame_barrier_camera: Camera2D
 @onready var felho_of_war_container: Node2D = $FelhoOfWarContainer
 
 @onready var starting_popup: Control = $StartingPopup
-
+@onready var world_shader_material: ShaderMaterial = $CanvasLayer/VignetteFilter.material as ShaderMaterial
 
 
 # Called when the node enters the scene tree for the first time.
@@ -37,31 +37,45 @@ func _on_all_checkpoints_collected():
 
 
 func trigger_barrier_remove() -> void:
+	# 0. Initial wait and pause
 	await get_tree().create_timer(0.6).timeout
 	get_tree().paused = true
 	
-	# 1. Start Camera Transition
-	CameraTransition.transition_camera(ship_camera, endgame_barrier_camera)
+	# 1. Start Camera Transition AND WAIT FOR IT
+	await CameraTransition.transition_camera(ship_camera, endgame_barrier_camera, 1.0)
 	
-	# 2. Fade OUT the felho container (after camera transition starts)
-	# We create a tween that runs even while the game is paused
+	# --- SHAKE & DESTRUCTION START ---
+	
+	# 2. Fade OUT the felho container
 	var tween_out = create_tween()
 	tween_out.set_pause_mode(Tween.TWEEN_PAUSE_PROCESS)
-	tween_out.tween_property(felho_of_war_container, "modulate:a", 0.0, 1.0) # Fades to alpha 0 over 1 second
+	tween_out.tween_property(felho_of_war_container, "modulate:a", 0.0, 1.0)
 	
 	endgame_barrier_destroy.emit()
+	# 4. START SHADER SHAKE (Animate the ShakeStrength uniform)
+	var shake_tween = create_tween()
+	shake_tween.set_pause_mode(Tween.TWEEN_PAUSE_PROCESS)
 	
-	await get_tree().create_timer(3).timeout
-	
-	# 3. Transition Camera Back
-	CameraTransition.transition_camera(endgame_barrier_camera, ship_camera)
-	
-	# 4. Fade IN the felho container
-	var tween_in = create_tween()
-	tween_in.set_pause_mode(Tween.TWEEN_PAUSE_PROCESS)
-	tween_in.tween_property(felho_of_war_container, "modulate:a", 1.0, 1.0) # Fades to alpha 1 over 1 second
+	# Instant maximum shake (1.0)
+	var max_shake_strength: float = 1
+	var fade_out_time = 3
 
-	await get_tree().create_timer(1).timeout
+	# MODIFICATION: Add .set_delay(1.0) to the first property tween
+	shake_tween.tween_property(world_shader_material, "shader_parameter/ShakeStrength", max_shake_strength, 0.01) \
+		.set_trans(Tween.TRANS_LINEAR) \
+		.set_delay(1.0) # <--- ADD THIS LINE FOR 1-SECOND DELAY
+	
+	# Then fade the shake out over the duration
+	shake_tween.tween_property(world_shader_material, "shader_parameter/ShakeStrength", 0.0, fade_out_time) \
+		.set_delay(0.01) # Hold max shake briefly
+	
+	# 5. Wait for the sequence to complete (Total wait: 1 second delay + 4 seconds duration)
+	await get_tree().create_timer(1.0 + fade_out_time, true, false, true).timeout
+	
+	# 6. Transition Camera Back
+	await CameraTransition.transition_camera(endgame_barrier_camera, ship_camera, 1.0)
+	
+	# 7. Unpause and Cleanup
 	get_tree().paused = false
 
 
