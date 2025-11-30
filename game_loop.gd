1ab6d5444a778fe9348b4368a9fbe929e85380d0
Csaba Ekart <csaba.ekart@gmail.com>
2025-11-30 19:19:19 +0100

    do the effect with fading

--- game_loop.gd
+++ game_loop.gd
@@ -18,7 +18,7 @@ func _ready() -> void:
 	Globals.go_to_last_checkpoint.connect(_on_go_to_last_checkpoint)
 	ship_camera = ship.get_camera()
 	endgame_barrier_camera = endgame_barrier.get_camera()
-	starting_popup.start()
+	# starting_popup.start()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 # print("fps: ", Engine.get_frames_per_second())
@@ -39,12 +39,28 @@ func _on_all_checkpoints_collected():
 func trigger_barrier_remove() -> void:
 	await get_tree().create_timer(0.6).timeout
 	get_tree().paused = true
-	felho_of_war_container.hide()
+	
+	# 1. Start Camera Transition
 	CameraTransition.transition_camera(ship_camera, endgame_barrier_camera)
+	
+	# 2. Fade OUT the felho container (after camera transition starts)
+	# We create a tween that runs even while the game is paused
+	var tween_out = create_tween()
+	tween_out.set_pause_mode(Tween.TWEEN_PAUSE_PROCESS)
+	tween_out.tween_property(felho_of_war_container, "modulate:a", 0.0, 1.0) # Fades to alpha 0 over 1 second
+	
 	endgame_barrier_destroy.emit()
+	
 	await get_tree().create_timer(3).timeout
+	
+	# 3. Transition Camera Back
 	CameraTransition.transition_camera(endgame_barrier_camera, ship_camera)
-	felho_of_war_container.show()  # maybe remove felhoOfWar so challenge part is without felhok
+	
+	# 4. Fade IN the felho container
+	var tween_in = create_tween()
+	tween_in.set_pause_mode(Tween.TWEEN_PAUSE_PROCESS)
+	tween_in.tween_property(felho_of_war_container, "modulate:a", 1.0, 1.0) # Fades to alpha 1 over 1 second
+
 	await get_tree().create_timer(1).timeout
 	get_tree().paused = false
 
