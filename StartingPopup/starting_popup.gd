872bbf82f26206ca48272eb4dbef3a9d32d9ee80
Dániel Gergő <danigerci@gmail.com>
2025-11-30 16:04:38 +0100

    Proper fade-in and voiceover.

--- StartingPopup/starting_popup.gd
+++ StartingPopup/starting_popup.gd
@@ -1,19 +1,38 @@
 extends Node2D
 
+var starting_dialog = preload("res://StartingPopup/0_Fisherman_22sec.mp3")
+
 @onready var texture: TextureRect = $CanvasLayer/ImagePopup/TextureRect
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready() -> void:
 	texture.modulate.a = 0
+
 	fade_in()
 
 
 func fade_in():
 	show()
 	var tween = create_tween()
-	tween.tween_property(texture, "modulate:a", 1.0, 1.0).set_ease(Tween.EASE_IN_OUT)
+	tween.tween_property(texture, "modulate:a", 1.0, 2.0).set_ease(Tween.EASE_IN_OUT)
+	await tween.finished
+	play_audio()
+
+
+func play_audio():
+	var audio_stream_player = AudioStreamPlayer.new()
+	add_child(audio_stream_player)
+	audio_stream_player.stream = starting_dialog
+	audio_stream_player.bus = "ShipLogSounds"
+	audio_stream_player.play()
+	var length = audio_stream_player.stream.get_length()
+	await get_tree().create_timer(length).timeout
+	audio_stream_player.queue_free()
+	fade_out()
+
 
 func fade_out():
 	var tween = create_tween()
-	tween.tween_property(texture, "modulate:a", 0.0, 0.3)
-	hide()
+	tween.tween_property(texture, "modulate:a", 0.0, 1.5)
+	await tween.finished
+	queue_free()
